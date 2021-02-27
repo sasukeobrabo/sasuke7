94a88956fe46e6a1ca1fe7571def7ac624470ec2
sasukeobrabo <79226499+sasukeobrabo@users.noreply.github.com>
2021-02-27 18:16:31 -0300

    Add files via upload

---
+++ Configurações/config.js
@@ -0,0 +1,6 @@
+const config = {
+        botName: 'SASUKE_BOT',
+        ownerName: 'SASUKE',
+        youtube: 'https://m.youtube.com/channel/UC6In9Gf-d8rJBvuwCzgYjbw',
+        instagram: 'https://www.instagram.com/_bielxsilva/',
+}
